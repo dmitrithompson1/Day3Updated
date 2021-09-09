725c33fdb55fb36db2cf70fc1f3052646095afc3
dmitrithompson1 <32120387+dmitrithompson1@users.noreply.github.com>
2021-09-09 12:09:14 -0400

    Initial commit

---
+++ EmployeesApi/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeesApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeesDataContext _context;
+
+        public EmployeesController(EmployeesDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("employees")]
+        public ActionResult GetAllEmployees()
+        {
+            var employees = _context.Employees
+                .Where(e => e.IsActive)
+                .ToList();
+            return Ok(employees);
+        }
+    }
+}
